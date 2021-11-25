04440c87276ae0df3a5055991f5c7c019dbccf4a
Luiz Henrique <7henrique18@gmail.com>
2021-11-25 15:21:24 -0300

    feat: init

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const setValueOfCreateDateInput = () => {
+  const createDateInput = document.getElementById("create-date");
+  const actualDate = new Date().toISOString().split("T")[0];
+  createDateInput.value = actualDate;
+};
+setValueOfCreateDateInput();
+
+const handleCreateNewTask = (e) => {
+  e.preventDefault();
+
+  // get values of inputs
+  const createDate = document.getElementById("create-date").value;
+  const limitDate = document.getElementById("limit-date").value;
+  const description = document.getElementById("description").value;
+
+  if (description < 10) {
+    // showToast("A descrição deve ter mais que 10 caracteres");
+  }
+
+  const task = {
+    createDate,
+    limitDate,
+    description,
+  };
+
+  console.log(task);
+
+  // const form = document.getElementById("create-new-task");
+};
