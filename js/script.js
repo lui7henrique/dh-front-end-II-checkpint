cf6fe9ab71851899eda5c5c0a7c12236d1551d20
alexandrecpedro <alexandrecpedro@gmail.com>
2021-11-27 22:47:13 -0300

    Dark Mode

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
 const changeBackground = (event) => {
   document.body.style.backgroundColor = event.checked ? "black" : "";
-  document.body.style.color = event.checked ? "white" : "";
-  document.getElementById("container").style.color = event.checked
-    ? "black"
-    : "";
+  document.getElementById("container").style.color = event.checked ? "black" : "";
 };
